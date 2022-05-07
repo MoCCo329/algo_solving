522262ee2740e8b3c143030ad5bcdb51d770d57a
김도현 <kd8317@gmail.com>
2022-05-08 01:50:06 +0900

    19238 G3

--- BEAKJOON/19238.py
+++ BEAKJOON/19238.py
@@ -2,29 +2,34 @@
 
 
 def findCustomer(si, sj):
+    if arr[si][sj] == 2:
+        return si, sj, 0
+
     res = []
+    minD = 100
     v = [[0] * N for _ in range(N)]
     v[si][sj] = 1
     q = []
     q.append([si, sj])
     while q:
         i, j = q.pop(0)
+        if minD < v[i][j]:
+            break
         for di, dj in [[-1, 0], [0, -1], [0, 1], [1, 0]]:
             ni, nj = i + di, j + dj
             if 0 <= ni < N and 0 <= nj < N and v[ni][nj] == 0 and arr[ni][nj] != 1:
-                if res and v[i][j] > res[0][2]:
-                    break
                 if arr[ni][nj] == 2:
-                    arr[ni][nj] = 0
-                    res.append([ni, nj, v[i][j]])
+                    res.append([ni, nj])
+                    minD = v[i][j]
+                    q.append([ni, nj])
+                    v[ni][nj] = v[i][j] + 1
                 else:
                     q.append([ni, nj])
                     v[ni][nj] = v[i][j] + 1
     if res:
-        res.sort(key=lambda x: (x[0], x[1]))
-        return res[0]
-    else:
-        return -1, -1, -1
+        res.sort(key = lambda x: (x[0], x[1]))
+        return res[0] + [minD]
+    return -1, -1, -1
 
 
 def findDestination(ci, cj, fi, fj):
@@ -37,7 +42,7 @@ def findDestination(ci, cj, fi, fj):
         for di, dj in [[-1, 0], [0, -1], [0, 1], [1, 0]]:
             ni, nj = i + di, j + dj
             if 0 <= ni < N and 0 <= nj < N and v[ni][nj] == 0 and arr[ni][nj] != 1:
-                if (ni, nj) == (fi, fj):
+                if ni == fi and nj == fj:
                     return v[i][j]
                 else:
                     q.append([ni, nj])
@@ -55,19 +60,15 @@ for _ in range(M):
     arr[ci - 1][cj - 1] = 2
     cList.append([ci - 1, cj - 1, fi - 1, fj - 1])
 
-
-while cList:
+while M:
     si, sj = start
     ci, cj, d1 = findCustomer(si, sj)
-    if d1 == -1:
-        B = -1
-        break
-    elif B > d1:
+    arr[ci][cj] = 0
+    if B > d1 and d1 != -1:
         B -= d1
     else:
         B = -1
         break
-    print(d1)
 
     for i in range(M):
         if cList[i][:2] == [ci, cj]:
@@ -77,15 +78,11 @@ while cList:
             break
 
     d2 = findDestination(ci, cj, fi, fj)
-    if d2 == -1:
-        B = -1
-        break
-    elif B >= d2:
+    if B >= d2 and d2 != -1:
         B -= d2
     else:
         B = -1
         break
-    print(d2)
 
     start = [fi, fj]
     B += d2 * 2
