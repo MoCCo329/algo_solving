bf72371e54ff1f1ed8866c9565c5302f9de469c4
김도현 <kd8317@gmail.com>
2022-06-05 03:40:19 +0900

    15644 G1

---
+++ BEAKJOON/15644.py
@@ -0,0 +1,74 @@
+# 15644. 구슬 탈출3 2022-06-03
+
+d_list = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+def dfs(cnt, D, s):
+    global ans, ans_list
+    chk = 0
+    if cnt == 10 or cnt >= ans:
+        return
+    else:
+        for d in range(4):
+            if d // 2 == D // 2:
+                continue
+            # 움직인 후 위치 찾기
+            di, dj = d_list[d]
+            balls.sort(key=lambda x: (x[d // 2]), reverse=(d % 2))
+            temp = [balls[i][::] for i in range(2)]
+            for k in range(2):
+                i, j = balls[k][:2]
+                arr[i][j] = "."
+                while True:
+                    ni = i + di
+                    nj = j + dj
+                    if arr[ni][nj] == ".":
+                        i, j = ni, nj
+                    elif arr[ni][nj] == "O":
+                        if balls[k][2] == "R":
+                            if chk != -1:
+                                chk = 1
+                            break
+                        else:
+                            chk = -1
+                            break
+                    else:
+                        arr[i][j] = balls[k][2][::]
+                        balls[k][0], balls[k][1] = i, j
+                        break
+
+            if chk == 0:
+                dfs(cnt + 1, d, s + [d])
+
+            # 배열 원상복구
+            for i in range(2):
+                arr[balls[i][0]][balls[i][1]] = "."
+                balls[i] = temp[i][::]
+                arr[temp[i][0]][temp[i][1]] = temp[i][2][::]
+
+            if chk == 1:
+                ans = cnt + 1
+                ans_list = s + [d]
+                return
+            elif chk == -1:
+                chk = 0
+
+
+N, M = map(int, input().split())
+arr = [list(map(str, input())) for _ in range(N)]
+balls = [0, 0]
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == "R":
+            balls[0] = [i, j, "R"]
+        elif arr[i][j] == 'B':
+            balls[1] = [i, j, "B"]
+
+ans = 11
+ans_list = []
+dfs(0, 4, [])
+if ans != 11:
+    print(ans)
+    ans_list_trans = ['U', 'D', 'L', 'R']
+    for direction in ans_list:
+        print(ans_list_trans[direction], end='')
+else:
+    print(-1)
