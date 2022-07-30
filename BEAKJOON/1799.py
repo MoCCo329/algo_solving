f4a0f1bfec67ae4ef3d0ea55e0a8a0f8c2b12667
김도현 <kd8317@gmail.com>
2022-07-31 00:52:27 +0900

    Update 1799 G1

--- BEAKJOON/1799.py
+++ BEAKJOON/1799.py
@@ -3,35 +3,46 @@
 
 # 시간초과
 
-def dfs(k, end, cnt):
-    global ans
-    if k == end:
-        ans = max(ans, cnt)
-        return
+def dfs(type, k, end, cnt):
+    global ans_b, ans_w
 
-    if ans - cnt > (end - k) * 2:
+    if k == end:
+        if type == 'black':
+            ans_b = max(ans_b, cnt)
+        else:
+            ans_w = max(ans_w, cnt)
         return
 
     for i in range(k, end):
-        if not v[available[i][0]] and not v[available[i][1]]:
-            v[available[i][0]] = 1
-            v[available[i][1]] = 1
-            dfs(i + 1, end, cnt + 1)
-            v[available[i][0]] = 0
-            v[available[i][1]] = 0
+        if type=='black':
+            temp = black
+        else:
+            temp = white
+        if not v[temp[i][0]] and not v[temp[i][1]]:
+            v[temp[i][0]] = 1
+            v[temp[i][1]] = 1
+            dfs(type, i + 1, end, cnt + 1)
+            v[temp[i][0]] = 0
+            v[temp[i][1]] = 0
 
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 v = [0] * (4 * N - 2)
 
-available = []
+black = []
+white = []
 for i in range(N):
     for j in range(N):
         if arr[i][j]:
-            available.append((i + j, - j + i + 3 * N - 2))
+            if (i + j) % 2:
+                black.append((i + j, - j + i + 3 * N - 2))
+            else:
+                white.append((i + j, - j + i + 3 * N - 2))
 
-available.sort()
-ans = 1
-dfs(0, len(available), 0)
-print(ans)
+ans_b, ans_w = 0, 0
+if black:
+    dfs('black', 0, len(black), 0)
+if white:
+    dfs('white', 0, len(white), 0)
+print(ans_b + ans_w)
