9afbe7960fdfa0fd7389a332689c12ca0be32ebd
김도현 <kd8317@gmail.com>
2022-08-05 00:26:53 +0900

    Update 10775 G2

--- BEAKJOON/10775.py
+++ BEAKJOON/10775.py
@@ -1,28 +1,11 @@
 # 10775. 공항  2022-08-04
 
 
-def dfs(k):
-    global ans, MAX
-
-    ans = max(ans, sum(v))
-
-    if k == MAX:
-        return
-
-    chk = 0
-    for i in range(1, arr[k] + 1):
-        if v[i]:
-            continue
-        v[i] = 1
-        chk = 1
-        dfs(k + 1)
-        v[i] = 0
-    else:
-        if chk == 0:
-            MAX = k
-            return
-    dfs(k + 1)
-
+def find(x):
+    if uf[x] == x:
+        return x
+    uf[x] = find(uf[x])
+    return uf[x]
 
 G = int(input())
 P = int(input())
@@ -30,8 +13,12 @@ arr = []
 for _ in range(P):
     arr.append(int(input()))
 
-v = [0] * (G + 1)
+uf = [i for i in range(G + 1)]
 ans = 0
-MAX = P
-dfs(0)
+for p in arr:
+    g = find(p)
+    if g == 0:
+        break
+    uf[g] = uf[g - 1]
+    ans += 1
 print(ans)
