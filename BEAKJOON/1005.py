19626cade0ee8b947f723edcafc54431bed9fbf7
김도현 <kd8317@gmail.com>
2023-01-11 00:14:51 +0900

    Update 1005 G3

--- BEAKJOON/1005.py
+++ BEAKJOON/1005.py
@@ -1,38 +1,35 @@
 # 1005. ACM Craft  2023-01-10
 
 
+import heapq
+
 T = int(input())
 for tc in range(1, T + 1):
     N, K = map(int, input().split())
     D_list = list(map(int, input().split()))
     out_deg = [[] for _ in range(N)]
-    in_deg = [[] for _ in range(N)]
+    in_deg = [0 for _ in range(N)]
     for _ in range(K):
         o, i = map(lambda x: int(x) - 1, input().split())
         out_deg[o].append(i)
-        in_deg[i].append(o)
+        in_deg[i] += 1
     w = int(input()) - 1
 
-    dp = []
+    hq = []
     for i in range(N):
-        if len(in_deg[i]) == 0:
-            dp.append((D_list[i], i, -1))
-    dp.sort()
+        if in_deg[i] == 0:
+            heapq.heappush(hq, (D_list[i], i))
 
+    while hq:
+        t, dist = heapq.heappop(hq)
 
-    while dp:
-        t, dist, src = dp.pop(0)
+        in_deg[dist] -= 1
 
-        if src != -1:
-            in_deg[dist].remove(src)
-
-        if len(in_deg[dist]) != 0: continue
+        if in_deg[dist] > 0: continue
 
         if dist == w:
             print(t)
             break
 
-        if len(out_deg[dist]) > 0:
-            for o in out_deg[dist]:
-                dp.append((t + D_list[o], o, dist))
-            dp.sort()
+        for o in out_deg[dist]:
+            heapq.heappush(hq, (t + D_list[o], o))
