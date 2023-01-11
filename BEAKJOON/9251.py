ae2dacb2b9a99744609e14c53c73ce691f018f7b
김도현 <kd8317@gmail.com>
2023-01-11 11:59:17 +0900

    9251 G5

---
+++ BEAKJOON/9251.py
@@ -0,0 +1,23 @@
+# 9251. LCS  2022-01-11
+
+
+A = input()
+B = input()
+A_L = len(A)
+B_L = len(B)
+
+dp = [[0] * A_L for _ in range(B_L)]
+if A[0] == B[0]: dp[0][0] = 1
+for j in range(1, A_L):
+    dp[0][j] = max(dp[0][j - 1], 1 if A[j] == B[0] else 0)
+for i in range(1, B_L):
+    dp[i][0] = max(dp[i - 1][0], 1 if A[0] == B[i] else 0)
+
+for i in range(1, B_L):
+    for j in range(1, A_L):
+        if B[i] == A[j]:
+            dp[i][j] = max(dp[i - 1][j - 1] + 1, dp[i - 1][j], dp[i][j - 1])
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[B_L - 1][A_L - 1])
