1339e2f71ad55da23e3d4d537d33944acf5de099
김도현 <kd8317@gmail.com>
2023-01-15 22:01:35 +0900

    12738 G2

---
+++ BEAKJOON/12738.py
@@ -0,0 +1,30 @@
+# 12738. 가장 긴 증가하는 부분 수열 3  2023-01-15
+
+
+def lower_bound(n):
+    s, e = 0, len(ans) - 1
+    min_idx = len(ans)
+
+    while s <= e:
+        m = (s + e) // 2
+
+        if n <= ans[m]:
+            e = m - 1
+            min_idx = min(min_idx, m)
+        else:
+            s = m + 1
+
+    return min_idx
+
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+ans = [arr[0]]
+for i in range(1, N):
+    if ans[-1] < arr[i]:
+        ans.append(arr[i])
+    elif ans[-1] > arr[i]:
+        ans[lower_bound(arr[i])] = arr[i]
+
+print(len(ans))
