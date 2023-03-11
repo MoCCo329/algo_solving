c43060a2a6a5ca6da2085cd7a22a894516d84b41
김도현 <kd8317@gmail.com>
2023-03-12 04:22:47 +0900

    7578 P5

--- BEAKJOON/7578.py
+++ BEAKJOON/7578.py
@@ -1,53 +1,40 @@
-# 13907. 세금  2023-03-12
+# 7578. 공장  2023-03-12
 
 
-import heapq
+import sys
+input = sys.stdin.readline
 
 
-def dijkstra(s):
-    dist = [1000000] * (V + 1)
-    path = [0] * (V + 1)
+N = int(input())
+arr = list(map(int, input().split()))
+iterator = map(int, input().split())
+hashMap = {next(iterator) : i for i in range(N)}
 
-    pq = []
-    heapq.heappush(pq, (0, s, s))
-    while pq:
-        now = heapq.heappop(pq)
-        if dist[pq[1]] != pq[0]: continue
+k = 1
+while k < N: k <<= 1
+st = [0] * (k << 1)
+ans = 0
 
-        path[now[1]] = now[2]
+for i in range(N):
+    j = hashMap[arr[i]]
 
-        for next_node in adj_list[now[1]]:
-            if path[next_node[0]]: continue
-            next_dist = now[0] + next_node[1]
-            if next_dist < dist[next_node[0]]:
-                pq.add((next_dist, next_node[0], now[1]))
+    temp = 0
+    s, e = k, k + j
+    while s <= e:
+        if s % 2 == 1:
+            temp += st[s]
+            s += 1
+        if e % 2 == 0:
+            temp += st[e]
+            e -= 1
+        s, e = s // 2, e // 2
 
-    path_count = [0] * (V + 1)
-    for i in range(1, V + 1):
-        while path[i] != i:
-            path_count += 1
-            i = path[i]
+    st[k + j] = 1
+    idx = (k + j) // 2
+    while idx > 0:
+        st[idx] = st[idx * 2] + st[idx * 2 + 1]
+        idx //= 2
 
-    return dist, path
+    ans += i - temp
 
-
-V, E, K = map(int, input().split())
-S, D = map(int, input().split())
-adj_list = [[] * (V  + 1)]
-for _ in range(E):
-    a, b, d = map(int, input().split())
-    adj_list[a].append((b, d))
-    adj_list[b].append((a, d))
-
-S_dist, S_path = dijkstra(S)
-D_dist, D_path = dijkstra(D)
-
-fee = 0
-for k in range(K + 1):
-    if k != 0:
-        fee += int(input())
-
-    min_idx = 0
-    min_v = 1000000
-    for i in range(1, V + 1):
-        
+print(ans)
