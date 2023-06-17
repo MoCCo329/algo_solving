86717f5b4893a252d4ccdf860e5f0d3ed9e82e56
MoCCo <kd8317@gmail.com>
2023-06-18 03:37:49 +0900

    Update 1647 G4

--- BEAKJOON/1647.py
+++ BEAKJOON/1647.py
@@ -1,14 +1,8 @@
 # 1647. 도시 분할 계획  2022-07-21
 
 
-import heapq
-N, M = map(int, input().split())
-q = []
-for _ in range(M):
-    a, b, w = map(int, input().split())
-    heapq.heappush(q, (w, a, b))
-
-uf = [i for i in range(N + 1)]
+import sys
+input = sys.stdin.readline
 
 
 def find(x):
@@ -20,20 +14,30 @@ def find(x):
 
 def union(x, y):
     X, Y = find(x), find(y)
-    if X > Y:
-        uf[X] = Y
-    else:
+    if lank[X] > lank[Y]:
         uf[Y] = X
+        lank[X] += lank[Y]
+    else:
+        uf[X] = Y
+        lank[Y] += lank[X]
 
 
-mst = []
+N, M = map(int, input().split())
+q = [list(map(int, input().split())) for _ in range(M)]
+q.sort(key=lambda x: x[2])
+
+uf = [i for i in range(N + 1)]
+lank = [1] * (N + 1)
+
+g_cnt = N
 ans = 0
-while q:
-    w, a, b = heapq.heappop(q)
+for i in range(M):
+    if g_cnt == 2:
+        break
+    a, b, w = q[i]
     if find(a) != find(b):
-        mst.append(w)
         union(a, b)
         ans += w
-        temp = w
+        g_cnt -= 1
 
-print(ans - temp)
+print(ans)
